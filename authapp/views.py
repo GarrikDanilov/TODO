a4710da87c8b28c08ecb868938694925bfc00507
GarrikDanilov <garrik_danilov@mail.ru>
2023-01-19 00:06:56 +0300

    add filters

--- authapp/views.py
+++ authapp/views.py
@@ -1,8 +1,10 @@
-from rest_framework.viewsets import ModelViewSet
+from rest_framework.viewsets import GenericViewSet
+from rest_framework import mixins
 from authapp.models import User
 from authapp.serializers import UserModelSerializer
 
 
-class UserModelViewSet(ModelViewSet):
+class UserModelViewSet(mixins.ListModelMixin, mixins.RetrieveModelMixin, 
+                           mixins.UpdateModelMixin, GenericViewSet):
     queryset = User.objects.all()
     serializer_class = UserModelSerializer
--- config/settings.py
+++ config/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'rest_framework',
     'corsheaders',
+    'django_filters',
     'authapp',
     'mainapp',
 ]
@@ -146,5 +147,8 @@ REST_FRAMEWORK = {
     ],
     'DEFAULT_PARSER_CLASSES': [
         'djangorestframework_camel_case.parser.CamelCaseJSONParser',
-    ]
+    ],
+    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
+    'PAGE_SIZE': 100,
+    'DEFAULT_FILTER_BACKENDS': ['django_filters.rest_framework.DjangoFilterBackend']
 }
